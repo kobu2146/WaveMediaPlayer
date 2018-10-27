a2a1599c5bec3366ea111b30d0a285b2efd555ff
kobu2146 <usekobu@gmail.com>
2018-10-27 20:53:07 +0300

    Visulation for music

--- app/src/main/java/com/wavemediaplayer/MainActivity.java
+++ app/src/main/java/com/wavemediaplayer/MainActivity.java
@@ -1,13 +1,76 @@
 package com.wavemediaplayer;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.audiofx.Visualizer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.wavemediaplayer.Visaulizer.VisualizerView;
+
 public class MainActivity extends AppCompatActivity {
 
+    VisualizerView mVisualizerView;
+    private MediaPlayer mMediaPlayer;
+    private Visualizer mVisualizer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mVisualizerView = (VisualizerView) findViewById(R.id.myvisualizerview);
+
+        initAudio();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isFinishing() && mMediaPlayer != null) {
+            mVisualizer.release();
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
+
+    private void initAudio() {
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+       mMediaPlayer = MediaPlayer.create(this, R.raw.test);
+
+        setupVisualizerFxAndUI();
+        // Make sure the visualizer is enabled only when you actually want to
+        // receive data, and
+        // when it makes sense to receive data.
+        mVisualizer.setEnabled(true);
+        // When the stream ends, we don't need to collect any more data. We
+        // don't do this in
+        // setupVisualizerFxAndUI because we likely want to have more,
+        // non-Visualizer related code
+        // in this callback.
+        mMediaPlayer
+                .setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        mVisualizer.setEnabled(false);
+                    }
+                });
+        mMediaPlayer.start();
+
+    }
+
+    private void setupVisualizerFxAndUI() {
+
+        // Create the Visualizer object and attach it to our media player.
+        mVisualizer = new Visualizer(mMediaPlayer.getAudioSessionId());
+        mVisualizer.setCaptureSize(Visualizer.getCaptureSizeRange()[1]);
+        mVisualizer.setDataCaptureListener(
+                new Visualizer.OnDataCaptureListener() {
+                    public void onWaveFormDataCapture(Visualizer visualizer,
+                                                      byte[] bytes, int samplingRate) {
+                        mVisualizerView.updateVisualizer(bytes);
+                    }
+
+                    public void onFftDataCapture(Visualizer visualizer,
+                                                 byte[] bytes, int samplingRate) {
+                    }
+                }, Visualizer.getMaxCaptureRate() / 2, true, false);
     }
 }
